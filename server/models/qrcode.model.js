3b9e79b3e4cea4e4c45e77489d550715d562a7dc
EmilLykke <emillykkeg@gmail.com>
2023-02-06 20:09:09 +0100

    First

---
+++ server/models/qrcode.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const qrSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    item: { type: String, required: true,},
+    qr: { type: String, required: true,},
+    description: { type: String, required: true,},
+    lastStatus: { type: Date, required: true,},
+
+}, {
+    timestamps:true,
+});
+
+const QrCode = mongoose.model("QrCode", qrSchema);
+
+module.exports = QrCode;
